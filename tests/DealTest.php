49033f6b12bb386d3d8d8119a3039e2469ad9b2b
Nitinai Supreeyaporn <nitinai.supreeyaporn@minstersoft.co.uk>
2020-12-21 09:53:56 +0000

    Added findDeals and deleteDeal.

--- tests/DealTest.php
+++ tests/DealTest.php
@@ -26,4 +26,28 @@ class DealTest extends TestCase
         $this->assertContainsOnlyInstancesOf(\Minstersoft\ActiveCampaign\Resources\Deal::class, $deals);
     }
 
+    public function test_find_deals()
+    {
+        $activeCampaign = new ActiveCampaign('', '123', $http = Mockery::mock('GuzzleHttp\Client'));
+
+        $criteria = [
+            'limit'  => '1',
+            'offset' => '0',
+            'orders' => [
+                'next-action' => 'DESC',
+            ],
+        ];
+
+        $http->shouldReceive('request')->once()->with('GET', 'deals', ['query' => $criteria])->andReturn(
+            $response = Mockery::mock('GuzzleHttp\Psr7\Response')
+        );
+
+        $response->shouldReceive('getStatusCode')->once()->andReturn(200);
+        $response->shouldReceive('getBody')->once()->andReturn('{"data": [{"key": "value"}]}');
+
+        $deals = $activeCampaign->findDeals($criteria);
+
+        $this->assertContainsOnlyInstancesOf(\Minstersoft\ActiveCampaign\Resources\Deal::class, $deals);
+    }
+
 }
